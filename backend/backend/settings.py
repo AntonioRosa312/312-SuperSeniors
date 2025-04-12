fce1a66db395fd08b43a5058cf2a4df8f4aa4197
BrandonWendt94 <brandonwendt94@gmail.com>
2025-04-12 13:04:15 -0400

    mongo/djongo wasnt working well with django, change and went with PostgreSQL, fixed error in login, dockerfile/yml/reqirements changed also, tested new db with model in new directory=core

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "backend.core"
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'backend'),
+        'USER': os.environ.get('POSTGRES_USER', 'backenduser'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'backendpass'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
