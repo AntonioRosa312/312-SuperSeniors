dc0d5cf9579a69b238d2b50c5638d139adf578b2
LoganBrefka <122587840+LoganBrefka@users.noreply.github.com>
2025-04-26 16:54:29 -0400

    me and Antonino add logger functionality back to main from branch

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "backend.middleware.request_logger.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -164,8 +165,9 @@ LOGGING = {
  'disable_existing_loggers': False,
  'formatters': {
      'custom': {
-         'format': '{asctime} | {levelname} | {message}',
+         'format': 'TimeStamp: {asctime} | {levelname} | {message}',
          'style': '{',
+         'datefmt': '%Y-%m-%d %H:%M:%S',  # Human-readable time format
      },
  },
  'handlers': {
