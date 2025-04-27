54a1fa831d74707a68324ceb65926ab1fe8691ef
Donald Hughey <donaldhughey@donalds-air-2.mynetworksettings.com>
2025-04-27 14:36:29 -0400

    push

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-339zyggrp^ia1rfy8zdnok@m8#*78tp5v=!lzfp68y)m8k7o^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "super-seniors.cse312.dev"]
 
 
 # Application definition
